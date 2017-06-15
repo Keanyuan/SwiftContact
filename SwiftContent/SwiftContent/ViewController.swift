cb4a28d2254ec316563aa26857d84cea73644255
Keanyuan <617293977@qq.com>
2017-06-15 11:14:36 +0800

    初始化

---
+++ SwiftContent/SwiftContent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftContent
+//
+//  Created by 祁志远 on 2017/6/15.
+//  Copyright © 2017年 祁志远. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
